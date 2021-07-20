ffcca2eda083a8d4c07e4cdf32a367f0650e7d38
Agent208 Site69 <agent208@site69.ru>
2021-07-20 22:17:10 +0300

    Initial commit

---
+++ action_ajax_form.php
@@ -0,0 +1,21 @@
+<?
+if((isset($_POST['name']) && $_POST['name']!="") && (isset($_POST['phone']) && $_POST['phone']!="")){ 
+        $to = 'b2b@afanasy.ru'; 
+        // $to = 'spiridonov.ilya81@gmail.com'; 
+        $subject = 'Заявка с сайта День Рождения'; //Загаловок сообщения
+        $message = '
+                <html>
+                    <head>
+                        <title>'.$subject.'</title>
+                    </head>
+                    <body>
+                        <p>Имя: '.$_POST['name'].'</p>
+                        <p>Телефон: '.$_POST['phone'].'</p> 
+                        <p>Город: '.$_POST['city'].'</p>                      
+                    </body>
+                </html>'; //Текст нащего сообщения можно использовать HTML теги
+        $headers  = "Content-type: text/html; charset=utf-8 \r\n"; //Кодировка письма
+        $headers .= "From: afanasiy.info <afanasiy.info>\r\n"; //Наименование и почта отправителя
+        mail($to, $subject, $message, $headers); //Отправка письма с помощью функции mail
+}
+?>
---
+++ js/main.js
@@ -0,0 +1,85 @@
+
+$(document).ready(function() {
+    const swiper = new Swiper('.swiper-container', {
+        // Optional parameters
+        loop: true,
+        spaceBetween: 30,
+    
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });   
+
+	$('input[name="phone"]').inputmask("+7(999)999-99-99");
+
+    $("#ajax_form").submit(function() { //устанавливаем событие отправки для формы с id=form
+        var form_data = $(this).serialize(); //собераем все данные из формы
+        $.ajax({
+        type: 'POST', //Метод отправки
+        url: 'action_ajax_form.php', //путь до php фаила отправителя
+        data: form_data,
+            success: function(data){ // сoбытиe пoслe удaчнoгo oбрaщeния к сeрвeру и пoлучeния oтвeтa
+            alert('Заявка успешно отправлена!'); 
+            }
+        });
+    });
+
+    const elSelectCustom = document.getElementsByClassName("js-selectCustom")[0];
+    const elSelectCustomValue = elSelectCustom.children[0];
+    const elSelectCustomOptions = elSelectCustom.children[1];
+    const defaultLabel = elSelectCustomValue.getAttribute("data-value");
+
+    // Listen for each custom option click
+    Array.from(elSelectCustomOptions.children).forEach(function (elOption) {
+        elOption.addEventListener("click", e => {
+            // Update custom select text too
+            elSelectCustomValue.textContent = e.target.textContent;
+            // Close select
+            elSelectCustom.classList.remove("isActive");
+
+            if ($('.selectCustom-trigger').text() == 'Тверь') {
+                $('.popup__address').text('УЛ. ВОССТАНИЯ ДЕСЕПТИКОНОВ,6 13:00');
+                $('input[name="city"]').val('Тверь');
+            } else if ($('.selectCustom-trigger').text() == 'Торжок') {
+                $('.popup__address').text('УЛ. Пушкина,10 18:00');
+                $('input[name="city"]').val('Торжок');
+            } else if ($('.selectCustom-trigger').text() == 'Лихославль') {
+                $('.popup__address').text('УЛ. Ленина,3 10:00');
+                $('input[name="city"]').val('Лихославль');
+            }
+        });
+    });
+
+    // Toggle select on label click
+    elSelectCustomValue.addEventListener("click", e => {
+    elSelectCustom.classList.toggle("isActive");
+    });
+
+    // close the custom select when clicking outside.
+    document.addEventListener("click", e => {
+    const didClickedOutside = !elSelectCustom.contains(event.target);
+    if (didClickedOutside) {
+        elSelectCustom.classList.remove("isActive");
+    }
+    });
+
+    $(document).ready(function() {   
+        $('.js-modal').click(function(e) {
+            e.preventDefault();
+            var popup = $('.popup');
+            $('#mask').fadeTo("fast",0.5);
+            popup.fadeIn(300); 
+        });
+        
+        $('.window .close').click(function (e) { 
+            e.preventDefault();
+            $('#mask, .window').hide();
+        }); 
+        $('#mask').click(function () {
+            $(this).hide();
+            $('.window').hide();
+        }); 
+    });
+
+});
