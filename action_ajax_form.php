c153ef72b1ac1735413135b5fdfa8d24a9b6f1e9
Agent208 Site69 <agent208@site69.ru>
2021-07-27 23:52:07 +0300

    2.0

--- action_ajax_form.php
+++ action_ajax_form.php
@@ -1,7 +1,12 @@
-<?
-if((isset($_POST['name']) && $_POST['name']!="") && (isset($_POST['phone']) && $_POST['phone']!="")){ 
+<?php
+
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$email = $_POST['email'];
+$city = $_POST['city'];
+
+if((isset($name) && $name!="") && (isset($phone) && $phone!="")){ 
         $to = 'b2b@afanasy.ru'; 
-        // $to = 'spiridonov.ilya81@gmail.com'; 
         $subject = 'Заявка с сайта День Рождения'; //Загаловок сообщения
         $message = '
                 <html>
@@ -9,14 +14,23 @@ if((isset($_POST['name']) && $_POST['name']!="") && (isset($_POST['phone']) && $
                         <title>'.$subject.'</title>
                     </head>
                     <body>
-                        <p>Имя: '.$_POST['name'].'</p>
-                        <p>Телефон: '.$_POST['phone'].'</p> 
-                        <p>E-mail: '.$_POST['email'].'</p> 
-                        <p>Город: '.$_POST['city'].'</p>                      
+                        <p>Имя: '.$name.'</p>
+                        <p>Телефон: '.$phone.'</p> 
+                        <p>E-mail: '.$email.'</p> 
+                        <p>Город: '.$city.'</p>                      
                     </body>
                 </html>'; //Текст нащего сообщения можно использовать HTML теги
         $headers  = "Content-type: text/html; charset=utf-8 \r\n"; //Кодировка письма
         $headers .= "From: afanasiy.info <afanasiy.info>\r\n"; //Наименование и почта отправителя
         mail($to, $subject, $message, $headers); //Отправка письма с помощью функции mail
+
+        $today = date("j-m-Y, H:i");
+
+        $file = 'users.csv';
+        $tofile = "'$name';'$phone';'$email';'$city';'$today'\n";
+        $bom = "\xEF\xBB\xBF";
+        file_put_contents($file, $bom . $tofile . file_get_contents($file));
+
+        $count = htmlentities(file_get_contents("count.txt"));
+        file_put_contents("count.txt", $count + 1);
 }
-?>
--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
         // Optional parameters
         loop: true,
         spaceBetween: 30,
-    
+        autoHeight: true,
         navigation: {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
@@ -13,56 +13,29 @@ $(document).ready(function() {
 
 	$('input[name="phone"]').inputmask("+7(999)999-99-99");
 
-    $("#ajax_form").submit(function() { //устанавливаем событие отправки для формы с id=form
+    $("#ajax_form").submit(function(e) { //устанавливаем событие отправки для формы с id=form 
+        e.preventDefault();
         var form_data = $(this).serialize(); //собераем все данные из формы
         $.ajax({
         type: 'POST', //Метод отправки
         url: 'action_ajax_form.php', //путь до php фаила отправителя
         data: form_data,
             success: function(data){ // сoбытиe пoслe удaчнoгo oбрaщeния к сeрвeру и пoлучeния oтвeтa
-            alert('Заявка успешно отправлена!'); 
+                alert('Заявка успешно отправлена!'); 
+                $('#mask, .window').hide();
+                $("#ajax_form")[0].reset();
+                var count = +$(".vip__counter-number").text();
+                $(".vip__counter-number").text(count + 1);
+                
+                
+                $('.vip__counter-number').spincrement({
+                    thousandSeparator: "",
+                    duration: 1200
+                });
+                show = false;
             }
         });
     });
-
-    // const elSelectCustom = document.getElementsByClassName("js-selectCustom")[0];
-    // const elSelectCustomValue = elSelectCustom.children[0];
-    // const elSelectCustomOptions = elSelectCustom.children[1];
-    // const defaultLabel = elSelectCustomValue.getAttribute("data-value");
-
-    // // Listen for each custom option click
-    // Array.from(elSelectCustomOptions.children).forEach(function (elOption) {
-    //     elOption.addEventListener("click", e => {
-    //         // Update custom select text too
-    //         elSelectCustomValue.textContent = e.target.textContent;
-    //         // Close select
-    //         elSelectCustom.classList.remove("isActive");
-
-    //         if ($('.selectCustom-trigger').text() == 'Тверь') {
-    //             $('.popup__address').text('УЛ. ВОССТАНИЯ ДЕСЕПТИКОНОВ,6 13:00');
-    //             $('input[name="city"]').val('Тверь');
-    //         } else if ($('.selectCustom-trigger').text() == 'Торжок') {
-    //             $('.popup__address').text('УЛ. Пушкина,10 18:00');
-    //             $('input[name="city"]').val('Торжок');
-    //         } else if ($('.selectCustom-trigger').text() == 'Лихославль') {
-    //             $('.popup__address').text('УЛ. Ленина,3 10:00');
-    //             $('input[name="city"]').val('Лихославль');
-    //         }
-    //     });
-    // });
-
-    // Toggle select on label click
-    // elSelectCustomValue.addEventListener("click", e => {
-    //     elSelectCustom.classList.toggle("isActive");
-    // });
-
-    // close the custom select when clicking outside.
-    // document.addEventListener("click", e => {
-    // const didClickedOutside = !elSelectCustom.contains(event.target);
-    // if (didClickedOutside) {
-    //     elSelectCustom.classList.remove("isActive");
-    // }
-    // });
 
     $(document).ready(function() {   
         $('.js-modal').click(function(e) {
@@ -81,5 +54,24 @@ $(document).ready(function() {
             $('.window').hide();
         }); 
     });
+
+    var show = true;
+    var countbox = ".vip__counter";
+    $(window).on("scroll load resize", function () {
+        if (!show) return false; // Отменяем показ анимации, если она уже была выполнена
+        var w_top = $(window).scrollTop(); // Количество пикселей на которое была прокручена страница
+        var e_top = $(countbox).offset().top; // Расстояние от блока со счетчиками до верха всего документа
+        var w_height = $(window).height(); // Высота окна браузера
+        var d_height = $(document).height(); // Высота всего документа
+        var e_height = $(countbox).outerHeight(); // Полная высота блока со счетчиками
+        if (w_top + 500 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
+            $('.vip__counter-number').css('opacity', '1');
+            $('.vip__counter-number').spincrement({
+                thousandSeparator: "",
+                duration: 1200
+            });
+            show = false;
+        }
+    }); 
 
 });
