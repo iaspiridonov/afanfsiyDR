ca38011f4136e4cc5f0236c8bd6c22c6f59bc17c
Agent208 Site69 <agent208@site69.ru>
2021-08-17 00:23:43 +0300

    V5

--- js/main.js
+++ js/main.js
@@ -55,24 +55,24 @@ $(document).ready(function() {
         }); 
     });
 
-    var show = true;
-    var countbox = ".vip__counter";
-    $(window).on("scroll load resize", function () {
-        if (!show) return false; // Отменяем показ анимации, если она уже была выполнена
-        var w_top = $(window).scrollTop(); // Количество пикселей на которое была прокручена страница
-        var e_top = $(countbox).offset().top; // Расстояние от блока со счетчиками до верха всего документа
-        var w_height = $(window).height(); // Высота окна браузера
-        var d_height = $(document).height(); // Высота всего документа
-        var e_height = $(countbox).outerHeight(); // Полная высота блока со счетчиками
-        if (w_top + 500 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
-            $('.vip__counter-number').css('opacity', '1');
-            $('.vip__counter-number').spincrement({
-                thousandSeparator: "",
-                duration: 1200
-            });
-            show = false;
-        }
-    }); 
+    // var show = true;
+    // var countbox = ".vip__counter";
+    // $(window).on("scroll load resize", function () {
+    //     if (!show) return false; // Отменяем показ анимации, если она уже была выполнена
+    //     var w_top = $(window).scrollTop(); // Количество пикселей на которое была прокручена страница
+    //     var e_top = $(countbox).offset().top; // Расстояние от блока со счетчиками до верха всего документа
+    //     var w_height = $(window).height(); // Высота окна браузера
+    //     var d_height = $(document).height(); // Высота всего документа
+    //     var e_height = $(countbox).outerHeight(); // Полная высота блока со счетчиками
+    //     if (w_top + 500 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
+    //         $('.vip__counter-number').css('opacity', '1');
+    //         $('.vip__counter-number').spincrement({
+    //             thousandSeparator: "",
+    //             duration: 1200
+    //         });
+    //         show = false;
+    //     }
+    // }); 
 
     
     $('#js-new-person--1').click(function () { 
